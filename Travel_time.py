7e51166e6b1de4e92a59b654ae13f9cb7a507c90
Libo <2878117430@qq.com>
2019-09-04 16:28:10 +0800

    分时段计算路段旅行时间

---
+++ Travel_time.py
@@ -0,0 +1,3 @@
+
+from jdbc.Connect import get_connection, free
+
---
+++ jdbc/Connect.py
@@ -0,0 +1,10 @@
+import cx_Oracle
+
+# 连接数据库
+def get_connection():
+    conn = cx_Oracle.connect('scott/cf6024584@localhost/orcl')  # 用户名/密码@服务器地址/数据库名称
+    return conn
+# 关闭连接
+def free(conn, cursor):
+    cursor.close()
+    conn.close()
