2f156f3afe738ec0dbce30d43098327d4f1c58d9
Libo <2878117430@qq.com>
2019-09-16 19:26:19 +0800

    高频车查询，已实现： （1）给定日期所在周的全部车辆上下学期间出行次数 （2）给定日期所在周的休息日全部车辆上下学期间出行次数

--- High Frequency Vehicle.py
+++ High Frequency Vehicle.py
@@ -15,7 +15,7 @@ def High_frequency_vehicles(conn, start_time):
     # 查询车牌号码和经过时间记录数（仅上下学期间）
     query_sql = ("SELECT HPHM, COUNT(JGSJ) FROM SJCJ_T_CLXX_LS WHERE SSID='HK-107' AND CDBH IN ('1','2','3','4') "
                  "AND TO_CHAR(JGSJ,'HH24') IN ('6','7','11','13','14','16','17')"
-                 " AND JGSJ BETWEEN to_date('%s','yyyy-mm-dd hh24:mi:ss') AND to_date('%s','yyyy-mm-dd hh24:mi:ss') ") % (query_time_interval[0], query_time_interval[1])
+                 " AND JGSJ BETWEEN to_date('%s','yyyy-mm-dd hh24:mi:ss') AND to_date('%s','yyyy-mm-dd hh24:mi:ss') GROUP BY HPHM ") % (query_time_interval[0], query_time_interval[1])
 
     cr.execute(query_sql)   # 执行查询
     query_res_total = cr.fetchall() # 查询结果从游标中提取并赋值给变量query_res_total
@@ -27,7 +27,7 @@ def High_frequency_vehicles(conn, start_time):
     for i in range(len(holiday_list)):
         query_sql_for_holiday = ("SELECT HPHM, COUNT(JGSJ) FROM SJCJ_T_CLXX_LS WHERE SSID='HK-107' AND CDBH IN ('1','2','3','4') "
                  "AND TO_CHAR(JGSJ,'HH24') IN ('6','7','11','13','14','16','17')"
-                 " AND JGSJ BETWEEN to_date('%s','yyyy-mm-dd hh24:mi:ss') AND to_date('%s','yyyy-mm-dd hh24:mi:ss') ") % (holiday_list[i], Add_serval_days(holiday_list[i], 1)) # 查休息日当天的出行情况
+                 " AND JGSJ BETWEEN to_date('%s','yyyy-mm-dd hh24:mi:ss') AND to_date('%s','yyyy-mm-dd hh24:mi:ss') GROUP BY HPHM ") % (holiday_list[i], Add_serval_days(holiday_list[i], 1)) # 查休息日当天的出行情况
         cr.execute(query_sql_for_holiday)   # 执行查询
         query_res_holiday = cr.fetchall()   # 查询结果提取
         To_dataframe = pd.DataFrame(list(query_res_holiday),columns=['HPHM', 'JGSJ_numbers'])   # 转为dataframe
@@ -36,7 +36,7 @@ def High_frequency_vehicles(conn, start_time):
     df_holiday = pd.concat(holiday_query_list, ignore_index=True)  # 将所有查询结果合并（上下合并）
     df_holiday = df_holiday.groupby('HPHM').sum().reset_index() # 按HPHM列的内容进行表内合并，对其他列执行求和的操作
 
-    #
+    # 将df_holiday和dataframe_res_total整合
 
 
 
