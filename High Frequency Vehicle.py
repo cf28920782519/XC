f4015f585d9ec369e539b241678ef0052352d441
Libo <2878117430@qq.com>
2019-09-16 17:14:21 +0800

    （1）上游查询东、南、北进口卡口流水 （2）新增按GJSJ_y列去重复项（保留最后出现的一项） （3）新增建表语句 （4）新增路中卡口相关内容

--- High Frequency Vehicle.py
+++ High Frequency Vehicle.py
@@ -10,10 +10,14 @@ def High_frequency_vehicles(conn, start_time):
 
     # 按日期查询一周内上下学期间车辆的出行次数，并返回出行次数大于等于5次的车牌号及其总体出行次数
 
-    query_time_interval = []
+    query_time_interval = Week_Period(start_time)
     query_sql = ("SELECT HPHM, COUNT(JGSJ) FROM SJCJ_T_CLXX_LS WHERE SSID='HK-107' AND CDBH IN ('1','2','3','4') "
                  "AND TO_CHAR(JGSJ,'HH24') IN ('6','7','11','13','14','16','17')"
-                 " AND JGSJ BETWEEN to_date('%s','yyyy-mm-dd') AND to_date('%s','yyyy-mm-dd') ")%()
+                 " AND JGSJ BETWEEN to_date('%s','yyyy-mm-dd hh24:mi:ss') AND to_date('%s','yyyy-mm-dd hh24:mi:ss') ") % (query_time_interval[0], query_time_interval[1])
+
+    cr.execute(query_sql)
+    query_res = cr.fetchall()
+    dataframe_res = pd.DataFrame(list(query_res),columns=['HPHM', 'JGSJ_numbers'])
 
 
 
--- jdbc/Start_End_time_list.py
+++ jdbc/Start_End_time_list.py
@@ -1,5 +1,6 @@
 import datetime
 from jdbc.Convert_strTo_time_then_str import Convert_strTo_time_then_str
+from jdbc.holiday import holiday
 
 
 # 返回2个二维数组，list中的每个元素示例：
@@ -20,12 +21,12 @@ def Start_End_time_list(start_time,date_length): # 输入start_time格式为'201
 
 def Week_Period(start_time):        # start: '2019-05-02 00:00:00'
     st_week_day = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S").weekday() # 输入的起始时间是周几
-    print(st_week_day)
+    # print(st_week_day)
     week_period = []
-    if st_week_day != 0:
+    if st_week_day != 0:    # 0-6是代表从周一到周日
         new_start_time = Convert_strTo_time_then_str(start_time, 1440*(7-st_week_day))
         week_period.append(new_start_time)
-        end_time = Convert_strTo_time_then_str(new_start_time,10080)
+        end_time = Convert_strTo_time_then_str(new_start_time, 10080)
         week_period.append(end_time)
     else:
         week_period.append(start_time)
@@ -34,6 +35,32 @@ def Week_Period(start_time):        # start: '2019-05-02 00:00:00'
 
     return week_period
 
+def Add_serval_days(start_time, date_length):   # start_time格式同上，date_length是个int类型，返回一个表征时间的字符串
+    end_time = Convert_strTo_time_then_str(start_time, 1440*date_length)
+    return end_time
+
+def Get_Holidays_during_Aweek(start_time):
+    st_week_day = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S").weekday()
+    if st_week_day != 0:
+        start_time = Convert_strTo_time_then_str(start_time, -1440*st_week_day)
+
+    holidays_list = []
+    for i in range(7):
+        tem = Add_serval_days(start_time, i)
+        if holiday(tem[0:10]) != 0:
+            holidays_list.append(tem)
+
+
+    return holidays_list
+
+
+
+
+
+
 if __name__ == '__main__':
     week_period = Week_Period('2019-05-01 00:00:00')
     print(week_period)
+
+    holiday_list = Get_Holidays_during_Aweek('2019-05-09 00:00:00')
+    print(holiday_list)
