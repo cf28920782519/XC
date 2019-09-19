19a2205edc84136c2bfaf9099a5f775f2bb06907
Libo <2878117430@qq.com>
2019-09-19 20:37:14 +0800

    旅行时间计算（6段路，4个子段）

--- Pick_ups_vehicles.py
+++ Pick_ups_vehicles.py
@@ -14,11 +14,22 @@ def Work_day_frequency_vehicles(conn):
     cr.execute(query_sql)  # 执行查询
     query_res_total = cr.fetchall()  # 查询结果从游标中提取并赋值给变量query_res_total
     df_res_total = pd.DataFrame(list(query_res_total),columns=['HPHM', 'TOTAL_NUM', 'WORK_NUM'])    # 查询结果转化成pd的dataframe
-    df_res = df_res_total.groupby('HPHM').sum().reset_index()
+    df_res = df_res_total.groupby('HPHM').sum() # 按HPHM分组合并，将其他列HPHM相同的值求和
+    df_res = df_res.drop(index=(df_res.loc[1.0*(df_res['WORK_NUM']/df_res['TOTAL_NUM']) < 0.9].index))    # 保留工作日出行频数占总频数的90%以上的车牌
+    df_res = df_res.sort_values(by=['TOTAL_NUM'], ascending=False).reset_index()    # 将上面的结果按降序排列
+
 
 
     return df_res
 
 if __name__ == '__main__':
+    starttime = datetime.datetime.now()  # 统计程序的开始时刻
+
     conn = None
-    print(Work_day_frequency_vehicles(conn))
+    df_res = Work_day_frequency_vehicles(conn)
+    df_res.to_csv('D:/Result.csv')
+    print(df_res)
+
+    endtime = datetime.datetime.now()
+    print("the program runs : %d s" % (endtime - starttime).seconds)
+
