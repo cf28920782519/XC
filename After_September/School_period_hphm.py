b989e69fc22eeb3fe81366c109eca1b29ee64148
Libo <2878117430@qq.com>
2019-10-14 18:22:41 +0800

    包含7、8月不出行，9月出行的计算逻辑

--- After_September/School_period_hphm.py
+++ After_September/School_period_hphm.py
@@ -6,7 +6,7 @@ import datetime
 
 # import pysnooper
 
-
+# 查询date之后的高频出行车辆的车牌号
 # @pysnooper.snoop(va='res')
 def get_high_fre_vehicles(conn, date):
     if conn == None:
@@ -21,15 +21,15 @@ def get_high_fre_vehicles(conn, date):
     for ele in res:
         lis_res.append(ele[0])
 
-    HPHM_during_school_period = []
-    for hphm in lis_res:
+    HPHM_during_school_period = []  # 上下学期间的高频车列表
+    for hphm in lis_res:    # 高频车列表中的全部车辆
         conn = None
-        month_list_tem = get_travel_month(conn, hphm)
-        flag = whether_school_period_drive(month_list_tem)
-        if flag == 1:
-            HPHM_during_school_period.append(hphm)
+        month_list_tem = get_travel_month(conn, hphm)   # 获取给定车牌的全部出行月份列表，具体代码见Get_travel_month.py
+        flag = whether_school_period_drive(month_list_tem)  # 判断是否为出行月份含9月但不含7、8月的车牌列表，具体代码见Get_travel_month.py
+        if flag == 1:   # 标记为1
+            HPHM_during_school_period.append(hphm)  # 添加到 上下学期间的高频车列表
         else:
-            continue
+            continue    # 继续循环，不操作
 
     return HPHM_during_school_period
 
